24d6d7575064462c5f98f40b0df07eff89aca17d
Pascal Henrich <pascalh.1505@gmail.com>
2025-04-29 15:38:49 +0200

    updated dataset and env

--- src/online/EnergyDataset.py
+++ src/online/EnergyDataset.py
@@ -1,32 +1,44 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+import numpy as np
 
 class EnergyDataset(Dataset):
     def __init__(self, energy_path: str, price_path: str, forecast_size: int, customer: int, mode: str):
         csv_data = pd.read_csv(energy_path, header=0)
+        csv_data['Date'] = pd.to_datetime(csv_data['Date'])
         csv_data['Price'] = pd.read_csv(price_path, header=0)['Price']
         csv_data.fillna(0, inplace=True)
+        self._batteryCapacity = self._calcBatteryCapacity(csv_data.loc[0:17519].reset_index(drop=True), customer)
         if mode == 'train':
             csv_data = csv_data.loc[0:17519].reset_index(drop=True)
         elif mode == 'eval':
             csv_data = (csv_data.loc[17520:35039]).reset_index(drop=True)
         elif mode == 'test':
             csv_data = (csv_data.loc[35040:52559]).reset_index(drop=True)
+        dates = csv_data['Date']
+        fractional_hours = dates.dt.hour + dates.dt.minute / 60.0
+        hour_sin = np.sin(2 * np.pi * fractional_hours / 24)
+        hour_cos = np.cos(2 * np.pi * fractional_hours / 24)
+
         self._data = pd.DataFrame({'prosumption': csv_data[f'prosumption_{customer}'],
                                    'load': csv_data[f'load_{customer}'],
                                    'pv': csv_data[f'pv_{customer}'],
-                                   'price': csv_data['Price']})
+                                   'price': csv_data['Price'],
+                                   'sin': hour_sin,
+                                   'cos': hour_cos})
         self._forecast_size = forecast_size
-        self._batteryCapacity = self._calcBatteryCapacity(csv_data.loc[0:17519].reset_index(drop=True), customer)
-    
+     
     def __len__(self):
         return len(self._data)-self._forecast_size
     
     def __getitem__(self, idx):
-        net_load = torch.tensor(self._data['prosumption'].loc[idx:idx+self._forecast_size].values, dtype=torch.float32)
+        idx = int(idx)
+        prosumption = torch.tensor(self._data['prosumption'].loc[idx:idx+self._forecast_size].values, dtype=torch.float32)
         price = torch.tensor(self._data['price'].loc[idx:idx+self._forecast_size].values, dtype=torch.float32)
-        return net_load, price
+        feature = np.array([self._data['sin'][idx:idx+self._forecast_size].to_numpy(),self._data['cos'][idx:idx+self._forecast_size].to_numpy()])
+        feature = torch.tensor(feature, dtype=torch.float32)
+        return prosumption, price, feature
     
     def _calcBatteryCapacity(self, csv_data_train, customer):
         prosumption = torch.tensor(csv_data_train[f'prosumption_{customer}'].values)
--- src/online/Environment.py
+++ src/online/Environment.py
@@ -30,18 +30,24 @@ class BatteryScheduling(EnvBase):
     def _reset(self, td_in):
         if td_in is None or td_in.is_empty():
            td_in = self._makeParams()
+        else:
+            td_in['params'] = self._makeParams()['params']
 
         step = torch.tensor(0, dtype=torch.int64)
-        prosumption_data, price_data = self._dataset[step]
+        prosumption_data, price_data, time_data = self._dataset[step]
         prosumption = prosumption_data[0].item()
         prosumption_forecast = prosumption_data[1:]
         price = price_data[0].item()
         price_forecast = price_data[1:]
+        time_feature = torch.tensor([time_data[0][0], time_data[1][0]])
 
         td_out = TensorDict(
             {
+                'step': step,
+                'time_feature': time_feature,
                 'soe': torch.tensor(0.0),
                 'prosumption': prosumption,
+                'prosumption_forecast': prosumption_forecast,
                 'price': price,
                 'price_forecast': price_forecast,
                 'cost': torch.tensor(0.0),
@@ -54,35 +60,36 @@ class BatteryScheduling(EnvBase):
 
 
     def _step(self, td_in):
-        action = td_in['action'].squeeze()
-        td_in['params', 'step'] += 1
-        step = td_in['params', 'step']
+        action = td_in['action'].squeeze().detach()
+        step = td_in['step'] + 1
         old_soe = td_in['soe']
         old_cost = td_in['cost']
         params = td_in['params']
 
-        prosumption_data, price_data = self._dataset[step]
+        prosumption_data, price_data, time_data = self._dataset[step]
         prosumption = prosumption_data[0]
         prosumption_forecast = prosumption_data[1:]
         price = price_data[0]
         price_forecast = price_data[1:]
+        time_feature = torch.tensor([time_data[0][0], time_data[1][0]])
 
         new_soe = torch.clip(old_soe + action, torch.tensor(0.0), params['battery_capacity'])
-        clipped_action = old_soe - new_soe
+        clipped_action = new_soe - old_soe
         penalty_soe  = torch.abs(action - clipped_action)
 
-        grid = prosumption - clipped_action
-        grid_buy = grid if grid > 0.0 else torch.tensor(0.0)
-        grid_sell = torch.abs(grid) if grid <= 0.0 else torch.tensor(0.0)
+        grid = prosumption + clipped_action
         
-        cost =  (grid_sell-grid_buy)*price
+        cost =  grid*price
         new_cost = old_cost + cost
-        reward = cost - penalty_soe
+        reward = -cost - penalty_soe
 
         td_out = TensorDict(
             {
+                'step': step,
+                'time_feature': time_feature,
                 'soe': new_soe,
                 'prosumption': prosumption,
+                'prosumption_forecast': prosumption_forecast,
                 'price': price,
                 'price_forecast': price_forecast,
                 'cost': new_cost,
@@ -93,6 +100,7 @@ class BatteryScheduling(EnvBase):
             batch_size=td_in.shape,
             device=td_in.device,
         )
+
         return td_out
     
     def make_composite_from_td(self, td):
@@ -109,16 +117,26 @@ class BatteryScheduling(EnvBase):
     
     def _makeSpecs(self, td_param):
         self.observation_spec = Composite(
+            step=Bounded(low=0,
+                         high=td_param['params', 'max_steps'],
+                         shape=(),
+                         dtype=torch.int64),
+            time_feature=Bounded(low=0,
+                            high=1,
+                            shape=(2,),
+                            dtype=torch.float32),
             soe=Bounded(low = 0,
                          high = td_param['params', 'battery_capacity'],
                          shape=(),
                          dtype=torch.float32),
             prosumption=Unbounded(dtype=torch.float32, 
                                   shape=()),
+            prosumption_forecast=Unbounded(dtype=torch.float32, 
+                                            shape=(10,)),
             price=Unbounded(dtype=torch.float32,
                             shape=()),
             price_forecast=Unbounded(dtype=torch.float32, 
-                                     shape=(48,)),
+                                     shape=(10,)),
             cost=Unbounded(dtype=torch.float32, 
                            shape=()),
             params=self.make_composite_from_td(td_param['params']),
@@ -138,7 +156,6 @@ class BatteryScheduling(EnvBase):
             {
                 'params': TensorDict(
                     {
-                        'step': torch.tensor(0, dtype=torch.int64),
                         'battery_capacity': self._dataset.getBatteryCapacity(),
                         'max_power': self._dataset.getBatteryCapacity()/2,
                         'max_steps': self._dataset.__len__()
