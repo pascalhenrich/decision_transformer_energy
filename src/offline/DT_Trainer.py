81e01eadbbbf396209413ec977e76d7a60621016
Pascal Henrich <pascalh.1505@gmail.com>
2025-06-11 22:37:18 +0200

    Updated for container

--- src/offline/DT_Trainer.py
+++ src/offline/DT_Trainer.py
@@ -1,6 +1,7 @@
-class Trainer():
+class DT_Trainer():
     def __init__(self, model, dataset):
         self.model = model
+        self.dataset = dataset
 
     def train_iteration(self, num_steps, curr_iter):
         self.model.train()
@@ -12,3 +13,5 @@ class Trainer():
 
 
     def train_step():
+        # get data from dataset
+        
